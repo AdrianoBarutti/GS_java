356972ee6a3c1bfbb76601198dd3f78b6e2e1040
AdrianoBarutti <adrianopessuto@hotmail.com>
2025-06-03 23:03:35 -0300

    Atualiza configuração do datasource e nome da aplicação no application.properties

--- src/main/java/com/stormguard/stormguard_api/StormGuardApplication.java
+++ src/main/java/com/stormguard/stormguard_api/StormGuardApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StormGuardApplication {
 
-	public String PORT = System.getenv("PORT") != null ? System.getenv("PORT") : "8080";
-
 	public static void main(String[] args) {
 		SpringApplication.run(StormGuardApplication.class, args);
 	}
