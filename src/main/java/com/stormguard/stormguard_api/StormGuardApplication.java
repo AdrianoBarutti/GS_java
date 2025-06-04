0c287bc0608c16a3262f96560f734f9673d31bb6
AdrianoBarutti <adrianopessuto@hotmail.com>
2025-06-03 23:46:55 -0300

    Atualiza configuração do pom.xml para definir versão do PostgreSQL e ajusta propriedades de datasource no application.properties

--- src/main/java/com/stormguard/stormguard_api/StormGuardApplication.java
+++ src/main/java/com/stormguard/stormguard_api/StormGuardApplication.java
@@ -1,13 +1,20 @@
 package com.stormguard.stormguard_api;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StormGuardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StormGuardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StormGuardApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner logDatasourceUrl(@Value("${spring.datasource.url:NOT_SET}") String url) {
+        return args -> System.out.println("Datasource URL (Spring): " + url);
+    }
 }
